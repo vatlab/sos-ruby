cf8236fd08b72a7150517e416ecf20ecafe39be9
Bo Peng <ben.bog@gmail.com>
2017-11-04 22:20:03 -0500

    Fix CI for sos-javascript

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(name = "sos-javascript",
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     install_requires=[
-          'sos',
+          'sos>=0.9.10.0',
           'sos-notebook',
       ],
     entry_points= '''
