eab86b6fc43b23e4e01d1fd3afb13f4e695aeb0f
Bo Peng <ben.bog@gmail.com>
2017-11-05 07:10:46 -0600

    Entering 0.9.10.1

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(name = "sos-javascript",
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     install_requires=[
-          'sos>=0.9.10.0',
+          'sos>=0.9.10.1',
           'sos-notebook',
       ],
     entry_points= '''
