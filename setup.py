685fc332af1c7b77cc0cbfb67cb7f118889d3d2a
Bo Peng <ben.bog@gmail.com>
2017-11-12 10:28:52 -0600

    Release sos-javascript 0.9.10.1

--- setup.py
+++ setup.py
@@ -55,8 +55,8 @@ setup(name = "sos-javascript",
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     install_requires=[
-          'sos>=0.9.10.1',
-          'sos-notebook',
+          'sos>=0.9.10.11',
+          'sos-notebook>=0.9.10.9',
       ],
     entry_points= '''
 [sos_actions]
