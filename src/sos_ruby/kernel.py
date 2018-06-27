971252407365dce4016573c4fa9fc4ee971780a3
Man Chong Leong <ml69@rice.edu>
2018-06-27 12:51:24 -0500

    fix bug for range variable in ruby

--- src/sos_ruby/kernel.py
+++ src/sos_ruby/kernel.py
@@ -22,7 +22,7 @@ def __Ruby_py_repr(obj)
   elsif obj.nil?
     "None"
   elsif obj.instance_of? Range
-    return "range(" + obj.min().inspect + "," + obj.max().inspect + ")"
+    return "range(" + obj.min().inspect + "," + (obj.max()+1).inspect + ")"
   elsif obj.instance_of? Array
     return obj.map { |indivial_var| __Ruby_py_repr(indivial_var) }
   end
